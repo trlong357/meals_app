e20f71f1eac3e5d4977653babafc67525f92a67f
Long Tran <longtran@Longs-MacBook-Pro.local>
2022-07-27 11:44:19 +0700

    categories screen UI

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2, // 200 width 300 height
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tony meals"),
       ),
-      children: [],
+      body: GridView(
+          padding: const EdgeInsets.all(25),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2, // 200 width 300 height
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: dummyCategories
+              .map(
+                (data) => CategoryItem(data.title, data.color),
+              )
+              .toList()),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'categories_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tony Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              subtitle1: const TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      home: const MyHomePage(),
+      home: const CategoriesScreen(),
     );
   }
 }
@@ -33,9 +50,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tony Meals'),
+        title: const Text('Tony Meals'),
       ),
-      body: Center(
+      body: const Center(
         child: Text("Navigation"),
       ),
     );
